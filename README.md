6accfa743eb5c99fb97e96bab5b38e027486e32d
Adam Pingel <pingel@gmail.com>
2022-05-06 15:46:07 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# axle.g8
+This is a [Giter8][g8] template for [Axle][axle].
+
+```
+sbt new axlelang/axle.g8
+```
+
+Template license
+----------------
+Written in 2022 by Adam Pingel.
+
+To the extent possible under law, the author(s) have dedicated all copyright and related
+and neighboring rights to this template to the public domain worldwide.
+This template is distributed without any warranty. See <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+[axle]: https://axle-lang.org/
---
+++ src/main/g8/project/Dependencies.scala
@@ -0,0 +1,41 @@
+import sbt._
+
+object Dependencies {
+
+  lazy val axle = Seq(
+    "org.axle-lang" %% "axle-core"  % "0.6.3",
+    "org.axle-lang" %% "axle-xml"   % "0.6.3",
+    "org.axle-lang" %% "axle-jung"  % "0.6.3",
+    "org.axle-lang" %% "axle-jblas" % "0.6.3",
+    "org.axle-lang" %% "axle-joda"  % "0.6.3"
+  )
+
+  // needed by axle-jung (and for unit conversions)
+  lazy val jung = Seq(
+    "net.sf.jung"            % "jung-visualization" % "2.1",
+    "net.sf.jung"            % "jung-algorithms"    % "2.1",
+    "net.sf.jung"            % "jung-api"           % "2.1",
+    "net.sf.jung"            % "jung-graph-impl"    % "2.1",
+    //"net.sf.jung"          % "jung-io"            % "2.1"
+  )
+
+  // for animations
+  lazy val monix = Seq(
+    "io.monix"               %% "monix-reactive"    % "2.3.0",
+    "io.monix"               %% "monix-cats"        % "2.3.0"
+  )
+
+  // needed by axle-joda
+  lazy val joda = Seq(
+    "joda-time"              % "joda-time"          % "2.9.4",
+    "org.joda"               % "joda-convert"       % "1.8.1"
+  )
+
+  // needed by axle-jblas
+  lazy val jblas = "org.jblas"              % "jblas"              % "1.2.4"
+
+  // needed by axle-xml
+  lazy val scalaXml = "org.scala-lang.modules" %% "scala-xml"         % "1.3.0"
+
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
+}
